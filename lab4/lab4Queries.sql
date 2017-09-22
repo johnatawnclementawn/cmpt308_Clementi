b5e17759d9c82aa930a8273efdfe7299003dc60c
Johnathan Clementi <johnclementi@148-100-155-173.FoxNet.marist.edu>
2017-09-22 16:30:27 -0400

    Completed all queries.  Have not tested queries 4-7, need to add values to cab.db to test functionalities and robustness of query statements.

--- lab4/lab4Queries.sql
+++ lab4/lab4Queries.sql
@@ -1,6 +1,6 @@
 -- Author: Johnathan Clementi --
 -- Prof: Alan Labouseur --
--- Date: 09/18/2017 --
+-- Date: 09/22/2017 --
 -- Assignment: DBS Lab 4 --
 -- File name: lab4Queries --
 
@@ -26,14 +26,11 @@ where aid in (select aid
 select distinct pid
 from orders
 where aid in (select aid
-              from agents
-              where aid in (select aid
-                            from orders
-                            where cid in (select cid
-                                          from customers
-                                          where city = 'Beijing')
-              				)
- 			)
+              from orders
+              where cid in (select cid
+                            from customers
+                            where city = 'Beijing')
+             )			
 order by pid desc;
 
 -- 3. Obtain the ID and name of customers who did not order thru agent 03. --
@@ -42,14 +39,52 @@ select cid, name
 from customers
 where cid in (select cid
               from orders
-              where aid in (select aid
+              where aid in (select distinct aid
                            from orders
                            where aid != 'a03')
              );
+             
+-- 4. Get customer id's for customers who ordered products 1 and 7 --
+
+select distinct cid
+from orders
+where pid in ('p01','p07');
 
 
+-- 5. Get product id's (ordered high-low) not ordered by any customers ordering through agents a02 or a03 --
+
+select pid
+from orders
+where cid in (select cid
+              from orders
+              where aid in (select aid
+                            from orders
+                            where not aid in ('a02', 'a03')
+                           )
+             )
+order by pid desc;
 
 
+-- 6. Get name, discount, and city of customers ordering through agents in Tokyo or New York --
+
+select name, discountpct, city
+from customers
+where cid in (select cid
+              from orders
+              where aid in (select aid
+                            from agents
+                            where city in ('Tokyo', 'New York')
+                           )
+             );
+
+-- 7. Select customers who have the same discount as any customers in Duluth or London --
+
+select name
+from customers
+where discountpct in (select discountpct
+              from customers
+              where city in ('Duluth', 'London')
+             );
 
 
-
+-- End Script.  Completed 16:30, 2017/09/22 by Johnathan Clementi --
